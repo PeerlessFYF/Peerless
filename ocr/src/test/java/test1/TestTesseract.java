c3cbf4d4009e2589b0f0a2c3398667fab5fc39e3
yuki <yuki@windows10.microdone.cn>
2018-04-03 00:51:08 +0800

    add file

--- ocr/src/test/java/test1/TestTesseract.java
+++ ocr/src/test/java/test1/TestTesseract.java
@@ -1,5 +1,10 @@
 package test1;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -7,7 +12,7 @@ import org.peerless.ocr.tesseract.Tess4jUtils;
 import org.peerless.utils.ScreenUtils;
 
 /**
- * Test Tesseract 
+ * Test Tesseract
  * 
  * @author yinfeng.feng
  */
@@ -24,22 +29,44 @@ public class TestTesseract {
 
 		String filePath = "D:/Peerless/imgs/" + yyyyMMdd + "/";
 		String fileName = yyyyMMddHHmm + ".png";
-		
+
 		int left = 10;
 		int top = 360;
 		int width = 400;
 		int height = 385;
 
 		ScreenUtils.captureScreen(filePath, fileName, left, top, width, height);
-		
+
 		// 2、OCR 图像识别
 		String imagePath = filePath + fileName;
 		String result = Tess4jUtils.readChar(imagePath);
-		String result2 = Tess4jUtils.readChar(imagePath, Tess4jUtils.CHI_SIM);
-		
+//		String result2 = Tess4jUtils.readChar(imagePath, Tess4jUtils.CHI_SIM);
+
 		System.out.println(result);
-		System.out.println(result2);
-		
+//		System.out.println(result2);
+
+		readText(result);
+	}
+
+	public static void readText(String str) {
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					new ByteArrayInputStream(str.getBytes(Charset.forName("utf8"))), Charset.forName("utf8")));
+			String line;
+			int count = 0;
+			while ((line = br.readLine()) != null) {
+				if (!line.trim().equals("")) {
+					if (count % 2 == 0) {
+						System.out.println("========>" + line);
+					} else {
+						System.out.println("====%====>" + line);
+					}
+					count++;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
